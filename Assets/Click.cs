62203b2d7e1e4eecac37a4a33304a68ff319d398
pollivie <pollivie@usc.edu>
2019-03-19 21:23:51 -0700

    eva
    eva

---
+++ Assets/Click.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour {
+    enum attack { Null, Attack, Defend, Grab };
+    public Click(float inX, float inY, int inType)
+    {
+        xPos = inX;
+        yPos = inY;
+        attackType = inType;
+    }
+    
+    float xPos;
+    float yPos;
+    int attackType;
+}
